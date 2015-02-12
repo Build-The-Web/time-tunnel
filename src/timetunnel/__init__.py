38342d04a03726ea0da11428f55eab61eefb3c8d
jhermann <jh@web.de>
2015-02-12 21:50:07 +0100

    project skeleton

---
+++ src/timetunnel/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=bad-whitespace
+""" timetunnel – An event collection and dispatching µservice.
+
+    time-tunnel accepts timestamped events with loosely defined semantics
+    over a set of common APIs, and then dispatches those events on a
+    best-effort basis to various event sinks according to a ruleset that
+    is part of the configuration.
+
+    The main goal is to offer clients a way to easily push their events to
+    one uniform event submission API of their choosing, and then go back and
+    concentrate on their actual task. Once those events hit the dispatcher,
+    they can be filtered and duplicated according to the needs of the event sinks.
+
+    There's a set of built-in implementations for both delivery APIs and
+    event sinks, and they can be extended with custom ones via a plugin system.
+
+    Copyright ⓒ  2015 1&1 Group
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+__url__             = 'https://github.com/Feed-The-Web/time-tunnel'
+__version__         = '0.1.0'
+__license__         = 'Apache 2.0'
+__author__          = 'Jürgen Hermann'
+__author_email__    = 'jh@web.de'
+
+
+__all__ = [
+    #'',
+    #'EventSource',
+    #'EventSink',
+]
---
+++ src/timetunnel/__main__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=bad-continuation
+""" Command line interface.
+"""
+# Copyright ⓒ  2015 1&1 Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import click
+
+APP_NAME = 'timetunnel'
+CONTEXT_SETTINGS = dict(
+    help_option_names=['-h', '--help'],
+)
+
+
+@click.group(context_settings=CONTEXT_SETTINGS)
+@click.option('-q', '--quiet', is_flag=True, default=False, help='Be quiet (show only errors).')
+@click.option('-v', '--verbose', is_flag=True, default=False, help='Create extra verbose output.')
+def cli(quiet=False, verbose=False): # pylint: disable=unused-argument
+    """Access and control the time-tunnel µservice."""
+    appdir = click.get_app_dir(APP_NAME)
+    #click.secho('appdir = {0}'.format(appdir), fg='yellow')
+
+
+@cli.command()
+@click.argument('message')
+def send(message):
+    """Submit a text event to the given or configured time-tunnel endpoint."""
+    click.echo('send - not implemented! args={0}'.format((message,)))
+
+
+if __name__ == "__main__": # imported via "python -m"?
+    cli()
