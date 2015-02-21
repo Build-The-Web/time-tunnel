2523df6f878ac838c89d59df641fa4773762a2f6
jhermann <jh@web.de>
2015-02-22 00:39:19 +0100

    :construction: Using rituals for common tasks, added a 'serve' task for Flask

--- tasks.py
+++ tasks.py
@@ -1,58 +1,18 @@
 # -*- coding: utf-8 -*-
-import os
-import glob
-import shlex
+# pylint: disable=wildcard-import, unused-wildcard-import, bad-continuation
+""" Project automation for Invoke.
+"""
 
 from invoke import run, task
-from setup import *
-
-@task(default=True)
-def help():
-    """Invoked with no arguments."""
-    run("invoke --help")
-    run("invoke --list")
-    print("Use 'invoke -h ‹taskname›' to get detailed help.")
+from rituals.invoke_tasks import * # pylint: disable=redefined-builtin
 
 
 @task
-def clean(docs=False, backups=False, bytecode=False, dist=False, all=False, venv=False, extra=''):
-    """Perform house-cleaning."""
-    patterns = ['build']
-    if docs or all:
-        patterns.append('docs/_build')
-    if dist or all:
-        patterns.append('dist')
-    if backups or all:
-        patterns.extend(['*~', '**/*~'])
-    if bytecode or all:
-        patterns.extend(['*.py[co]', '**/*.py[co]'])
-
-    if venv:
-        patterns.extend(['bin', 'include', 'lib', 'share', 'local'])
-    if extra:
-        patterns.extend(shlex.split(extra))
-
-    for pattern in patterns:
-        for name in glob.iglob(pattern):
-            run('rm -rf "{0}"'.format(name))
-
+def serve():
+    """Start development web server."""
+    run("vortex runserver")
 
 @task
-def build(docs=False):
-    """Build the project."""
-    run("python setup.py build")
-    if docs and os.path.exists(srcfile("docs", "conf.py")):
-        run("sphinx-build docs docs/_build")
-
-
-@task
-def check():
-    """Perform source code checks."""
-    run('pylint "{0}"'.format(srcfile('src', project['name'])))
-
-
-@task
-def ci():
+def ci(): # pylint: disable=invalid-name
     """Perform continuous integration tasks."""
-    run("invoke clean --all build --docs check")
-
+    run("invoke clean --all build --docs test check")
